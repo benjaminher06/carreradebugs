b3935506a2e26d0b4f21378baab4ff58d0813a4b
benjaminher06 <fbenjaminsh@gmail.com>
2025-05-05 22:07:35 -0300

    Add files via upload

---
+++ arraybug.js
@@ -0,0 +1,11 @@
+
+  let fruit = [(manzana, banana, naranja)];
+frutas.push("pera");
+frutas.shift("mandarina");
+frutas.unshift("sandía");
+frutas.pop()
+console.log("Frutas:", frut);
+frutas.forEach((function(fruta, index)): 
+console.log(`Fruta en posición ${index}: ${fruta}`);
+);
+ 
